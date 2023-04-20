b8038c4a030509d9a671d0e2fd8eddd1f222004a
jgonzalez-89 <kbcerverjo@gmail.com>
2023-04-20 22:48:03 +0200

    So many updates ...

--- launcher.py
+++ launcher.py
@@ -1,3 +1,6 @@
+
+## v1.2 Lanza Scripts Multithreading en diferentes nucleos
+
 import os
 import sys
 import subprocess
@@ -8,30 +11,23 @@ import concurrent.futures
 aranas = ['ohpeluqueros', 'pcprofesional', 'goodcarecosmetics', 'levanitashop',
           'luileibeauty', 'dudebeauty', 'kapylook', 'hairlowers', 'corradoequipe']
 
-
-# # v1.2 Lanza Scripts Multithreading en diferentes nucleos
-
 def lanzar_arana(arana):
     comando = f'scrapy crawl {arana} -o {arana}.csv'
     subprocess.run(comando, shell=True)
-
 
 def lanzar_aranas():
     with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
         executor.map(lanzar_arana, aranas)
 
-
 def crear_carpeta(ruta_carpeta):
     if not os.path.exists(ruta_carpeta):
         os.makedirs(ruta_carpeta)
-
 
 def mover_archivos(nombres_archivos, origen, destino):
     for nombre_archivo in nombres_archivos:
         ruta_archivo_origen = os.path.join(origen, nombre_archivo)
         ruta_archivo_destino = os.path.join(destino, nombre_archivo)
         shutil.move(ruta_archivo_origen, ruta_archivo_destino)
-
 
 ruta_actual = os.path.dirname(os.path.abspath(__file__))
 entorno = os.path.join(ruta_actual, 'venv', 'Scripts', 'activate_this.py')
@@ -42,8 +38,6 @@ with open(entorno) as file_:
 ruta_proyecto_scrapy = os.path.join(ruta_actual, 'src')
 os.chdir(ruta_proyecto_scrapy)
 
-lanzar_aranas()
-
 nombres_archivos_salida = [f'{arana}.csv' for arana in aranas]
 
 # Crear una carpeta con la fecha actual (formato YYYY-MM-DD) en la raíz del proyecto
@@ -51,10 +45,15 @@ fecha_actual = datetime.now().strftime('%Y-%m-%d')
 ruta_carpeta_output = os.path.join(ruta_actual, fecha_actual)
 crear_carpeta(ruta_carpeta_output)
 
-# Mover los archivos de salida a la carpeta con la fecha actual
-mover_archivos(nombres_archivos_salida,
-               ruta_proyecto_scrapy, ruta_carpeta_output)
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()
 
+    lanzar_aranas()
+
+    # Mover los archivos de salida a la carpeta con la fecha actual
+    mover_archivos(nombres_archivos_salida,
+                   ruta_proyecto_scrapy, ruta_carpeta_output)
 
 # # v1.1 Lanza Scripts Multithreading
 
